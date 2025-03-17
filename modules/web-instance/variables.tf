8b69167aeb8a2a1b97b4f4ab66190cd07bc2ebb9
Illiathebest <70669620+Illiathebest@users.noreply.github.com>
2025-03-17 11:25:55 +0200

    Week4: Modules and IAM

--- modules/web-instance/variables.tf
+++ modules/web-instance/variables.tf
@@ -1,36 +1,32 @@
-variable "aws_region" {
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "ami_id" {
   type        = string
-  default     = "ami-04b4f1a9cf54c11d0"
 }
 
 variable "instance_type" {
   type        = string
-  default     = "t2.micro"
 }
 
 variable "key_name" {
   type        = string
-  default     = "devops"
 }
 
 variable "allowed_ssh_ip" {
   type        = string
-  default     = "95.158.53.82/32"
 }
 
 variable "ssh_user" {
   type        = string
-  default     = "ubuntu"
 }
 
 variable "private_key_path" {
   type        = string
-  default     = "C:/Users/kukha/Terraform/devops.pem"
 }
 
+variable "sg_name" {
+  type        = string
+  default     = "web_sg"
+}
 
+variable "aws_region" {
+  type        = string
+}
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+  value = module.web_instance.instance_public_ip
 }
 
 output "account_id" {
@@ -7,6 +7,6 @@ output "account_id" {
 }
 
 output "my_test_password" {
-  value       = jsondecode(aws_secretsmanager_secret_version.my_test_secret_version.secret_string).password
-  sensitive   = true
+  value     = jsondecode(aws_secretsmanager_secret_version.my_test_secret_version.secret_string).password
+  sensitive = true
 }
